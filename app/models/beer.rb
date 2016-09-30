73dc488db13e5d86aeaad4da13dfa1bd11c0f352
user <user@debian>
2016-09-30 12:57:27 +0200

    on va tenter de merger inchallah

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,22 @@
 class Beer < ActiveRecord::Base
 belongs_to :festival
-has_many :beer_geeks, through : :notes
+has_many :notes
+has_many :beer_geeks, through: :notes
+
+
+
+  def moyenne
+	moye=0
+        if notes.nil?
+		return
+	else
+		notes.each do |n|
+	      		moye=moye+n.star
+       	        end
+			moye=moye/notes.count
+	end
+	#notes.map { |note| note.star }.sum / notes.count unless notes.empty?
+  end
+
+
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :beer_geek
   belongs_to :beer
+  validates:beer_geek,presence:true
+  validates:beer,presence:true
 end
